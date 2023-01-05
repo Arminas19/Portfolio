e5f8e3a03ec30690393a776995f2c2339b6238a5
Arminas19 <armeska14@gmail.com>
2023-01-05 15:34:01 +0000

    Started on my portfolio project, finished working and designing the navbar

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,36 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import  Contact  from './Contact';
+import  Home  from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <div id='Navbar-div'>
+          <nav id='Navbar'>
+            <div id='Navbar-div-left'>
+              <h2 id='Navbar-name'> ARMINAS <br/> <span id='Navbar-surename'> ALINIKOVAS </span> </h2>
+            </div>
+            <div id='Navbar-div-center'>
+              <div>
+                <img id='Navbar-image' src="logo192.png" alt="Navbar image" width='50' height='50'/>
+                <h3 id='navbar-portfolio'> PORTFOLIO WEBSITE </h3>
+              </div>
+            </div>
+            <div id='Navbar-div-right'>
+              <div className='links'>
+                <Link to='/' > Home </Link>
+                <Link to='/contact'> Contact </Link>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
