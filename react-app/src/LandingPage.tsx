c9a14f06b36338556f9b9e0326920b704f25b9e8
Arminas19 <armeska14@gmail.com>
2023-01-14 16:40:26 +0000

    started on landing page

--- react-app/src/LandingPage.tsx
+++ react-app/src/LandingPage.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import './App.css';
+import './landingPage.css';
 
 export default function LandingPage() {
   return (
-    <div className='App'>
-        <div id='landingPage'>
-            LandingPage
+    <>
+      <body>
+        <div id="landingPageApp">
+            <div className='container'>
+              <div className='hero-content'>
+                <span className='hero-span'> Arminas Aleinikovas </span>
+                <h1 className='hero-h1'> Junior Web Developer</h1>
+                <p className='banner-para'> Pivoting my career towards Software Development is an exciting process. 
+                  At this present time I’m working as a freelancer with a senior Developer. 
+                  If you would like to know more about my current position, please feel free to contact me.   
+                </p>
+                  
+                <div className='button-container'>
+                  <div>
+                    <a className='hero-a btn-1' href="/Home"> Website </a>
+                  </div>
+                  <div>
+                    <a className='hero-a btn-2' href="Resume.docx"  download> Resume </a>
+                  </div>
+                </div>
+              </div>
+          </div>
         </div>
-    </div>
+      </body>
+
+    </>
   );
 };
