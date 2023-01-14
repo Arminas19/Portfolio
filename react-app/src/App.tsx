1f73dd96d9c96956c05b8fd02feba1e3f1ab7170
Arminas19 <armeska14@gmail.com>
2023-01-14 12:46:31 +0000

    created a fully responsive nav

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import  Contact  from './components/Contact';
 import  Home  from './Home';
+import LandingPage from './LandingPage';
 import './App.css';
 
 function App() {
@@ -10,24 +11,26 @@ function App() {
         <div id='Navbar-div'>
           <nav id='Navbar'>
             <div id='Navbar-div-left'>
-              <h2 id='Navbar-name'> ARMINAS <br/> <span id='Navbar-surename'> ALINIKOVAS </span> </h2>
+              <a href="/" style={{textDecoration:'none'}}><h2 id='Navbar-name'> ARMINAS <br/> <span id='Navbar-surename'> ALINIKOVAS </span> </h2></a>
             </div>
             <div id='Navbar-div-center'>
               <div>
                 <img id='Navbar-image' src="logo1.png" alt="Navbar image" width='50' height='50'/>
-                <h3 id='navbar-portfolio'> PORTFOLIO WEBSITE </h3>
+                <a href='/Home' className='portfolio-link' style={{textDecoration:'none'}}><h3 id='navbar-portfolio'> PORTFOLIO WEBSITE</h3></a>
               </div>
             </div>
             <div id='Navbar-div-right'>
               <div className='links'>
-                <Link to='/' > Home </Link>
-                <Link to='/contact'> Contact </Link>
+
+                <Link to='/Home' > Home </Link>
+                <Link to='/' > Profile </Link>
               </div>
             </div>
           </nav>
         </div>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={<LandingPage/>} />
+          <Route path='/Home' element={<Home />}/>
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
---
+++ react-app/src/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function LandingPage() {
+  return (
+    <div className='App'>
+        LandingPage
+    </div>
+  );
+};
