90ffd92a757f80987afbf56e654dcac1e6447b5f
Arminas19 <armeska14@gmail.com>
2023-01-14 15:05:36 +0000

    made contact form responsive

--- react-app/notes.md
+++ react-app/notes.md
@@ -1,3 +1,2 @@
 To Do List:
-1. make Contact Form Responsive.
 2. Create a landing page with a hero section With a few buttons going to my portfolio page or contact form.
--- react-app/src/components/Contact.tsx
+++ react-app/src/components/Contact.tsx
@@ -28,31 +28,35 @@ export default function Contact() {
   return (
   <div id='contact'>
     <div className="container">
-      <div className='row'>
-        <div className='form-left'>
-        <h1> Contact Me </h1>
-        <p> <i className="fa fa-paper-plane"></i>armeska14@gmail.com </p>
-        <p> <i className="fa fa-phone-square"></i>+353 89 478 7029 </p>
-        <div className='social-icons'>
-          <a href="https://www.linkedin.com/in/arminas-aleinikovas-0baa18220/" target='_blank'><i className='fab fa-linkedin'></i></a>
-          <a href="#Top"><i className="fa fa-home"></i></a>
-          <a href="/"><i className="fa fa-user"></i></a>
-          <a href="Cover Letter Arminas Aleinikovas" download><i className="fa fa-file-powerpoint-o"></i></a>
+      <div className='row'> 
+        <div className="col">
+          <div className='form-left'>
+          <h1> Contact Me </h1>
+          <p> <i className="fa fa-paper-plane"></i>armeska14@gmail.com </p>
+          <p> <i className="fa fa-phone-square"></i>+353 89 478 7029 </p>
+          <div className='social-icons'>
+            <a href="https://www.linkedin.com/in/arminas-aleinikovas-0baa18220/" target='_blank'><i className='fab fa-linkedin'></i></a>
+            <a href="#Top"><i className="fa fa-home"></i></a>
+            <a href="/"><i className="fa fa-user"></i></a>
+            <a href="Cover Letter Arminas Aleinikovas" download><i className="fa fa-file-powerpoint-o"></i></a>
+          </div>
+          <a href="Resume.docx"  download className='btn btn2'>Download CV</a>
+          </div>
         </div>
-        <a href="Resume.docx"  download className='btn btn2'>Download CV</a>
 
-        </div>
-        <div className="form-right">
-          <form onSubmit={handleSubmit}>
+        <div className="col">
+          <div className="form-right">
+            <form onSubmit={handleSubmit}>
 
-            <input placeholder="Full name" value={name} onChange={(event) => setName(event.target.value)}/>
+              <input placeholder="Full name" value={name} onChange={(event) => setName(event.target.value)}/>
 
-            <input placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+              <input placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}/>
 
-            <textarea placeholder="Message" value={message} onChange={(event) => setMessage(event.target.value)}/>
+              <textarea placeholder="Message" value={message} onChange={(event) => setMessage(event.target.value)}/>
 
-            <button className='btn btn2'> Submit Form </button>
-          </form>
+              <button className='btn btn2'> Submit Form </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
